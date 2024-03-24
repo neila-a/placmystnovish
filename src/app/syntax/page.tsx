fe00d2e1a6f39598130f297993afdff2fbf3e55d
Neila <neilaspace@outlook.com>
2024-03-24 16:00:57 +0800

    ✨

--- src/app/syntax/page.tsx
+++ src/app/syntax/page.tsx
@@ -28,12 +28,7 @@ export default function Syntax() {
             }}>
                 {get("syntaxpage.suffix")}
             </Typography>
-            <Typography variant="subtitle1" sx={{
-                mb: 2
-            }}>
-                {get("syntaxpage.convert")}
-            </Typography>
-            {([["adjective", adjectives], ["number", numbers], ["noun", nouns], ["predicate", predicates]] as [string, [string, string][]][]).map(item => <Box component="section" sx={{
+            {([["adjective", adjectives], ["number", numbers], ["predicate", predicates]] as [string, [string, string][]][]).map(item => <Box component="section" sx={{
                 mb: 4
             }} key={item[0]}>
                 <Typography variant="h4" sx={{
@@ -100,6 +95,44 @@ export default function Syntax() {
                                     </TableCell>
                                     <TableCell align="right">
                                         {verb[2]}
+                                    </TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            </Box><Box component="section" sx={{
+                mb: 4
+            }}>
+                <Typography variant="h4" sx={{
+                    mb: 2
+                }}>
+                    {get("syntaxpage.noun")}
+                </Typography>
+                <TableContainer component={Paper}>
+                    <Table aria-label="noun">
+                        <TableHead>
+                            <TableRow>
+                                <TableCell>{get("syntaxpage.case")}</TableCell>
+                                <TableCell align="right">{get("syntaxpage.a")}</TableCell>
+                                <TableCell align="right">{get("syntaxpage.s")}</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {(nouns as [string, string, string][]).map(noun => (
+                                <TableRow key={noun[0]} sx={{
+                                    '&:last-child td, &:last-child th': {
+                                        border: 0
+                                    }
+                                }}>
+                                    <TableCell component="th" scope="row">
+                                        {get(`syntaxpage.${noun[0]}`)}
+                                    </TableCell>
+                                    <TableCell align="right">
+                                        {noun[1]}
+                                    </TableCell>
+                                    <TableCell align="right">
+                                        {noun[2]}
                                     </TableCell>
                                 </TableRow>
                             ))}
