71b0711b302828e5bb8fa3965a18b8ef9b44add9
Neila <neilaspace@outlook.com>
2024-07-06 13:22:14 +0800

    ✨ l在p或b后变为ɾ

--- src/app/[lang]/index/PagesGrid/container.tsx
+++ src/app/[lang]/index/PagesGrid/container.tsx
@@ -11,6 +11,7 @@ export default function PagesGridContainer(props: {
     children: ReactNode;
 }) {
     const theme = useTheme(),
+        // 上下文中没有theme，不能使用queryFC
         downsm = useMediaQuery(theme.breakpoints.down("sm"));
     return <Grid container component="section" direction={downsm ? "column" : "row"} sx={{
         justifyContent: "space-between"
